a808ac7597e37fbd11bf54d2367e68223fcaee93
Jason Felice <jason.m.felice@gmail.com>
2013-07-17 21:55:06 -0400

    P/EQ

--- src/parser.lisp
+++ src/parser.lisp
@@ -3,9 +3,15 @@
   (:use :cl))
 (in-package :cl-infix-parser)
 
-(export 'p/number)
+(export '(p/number p/eq))
 
 (defun p/number (tokens)
   (if (numberp (car tokens))
     (values t (car tokens) (cdr tokens))
     (values)))
+
+(defun p/eq (value)
+  #'(lambda (tokens)
+      (if (eq (car tokens) value)
+	(values t (car tokens) (cdr tokens))
+	(values))))
--- t/parser.lisp
+++ t/parser.lisp
@@ -8,23 +8,27 @@
 (defmacro parse (parser tokens &rest rest)
   (ecase (first rest)
     (:fails
-     `(multiple-value-bind (parse-ok) (,parser ',tokens)
+     `(multiple-value-bind (parse-ok) (funcall ,parser ',tokens)
         (ok (not parse-ok))))
     (:returns
-     `(multiple-value-bind (parse-ok result) (,parser ',tokens)
+     `(multiple-value-bind (parse-ok result) (funcall ,parser ',tokens)
 	(if parse-ok
-	  (is result ,(second rest))
+	  (is result ',(second rest))
 	  (ok nil))))
     (:leaves
-     `(let ((parse-result (multiple-value-list (,parser ',tokens))))
+     `(let ((parse-result (multiple-value-list (funcall ,parser ',tokens))))
 	(if (first parse-result)
 	  (is (third parse-result) ',(second rest))
 	  (ok nil))))))
 
-(plan 3)
+(plan 6)
 
-(parse p/number () :fails)
-(parse p/number (42 x) :returns 42)
-(parse p/number (42 x) :leaves (x))
+(parse #'p/number () :fails)
+(parse #'p/number (42 x) :returns 42)
+(parse #'p/number (42 x) :leaves (x))
+
+(parse (p/eq 'y) () :fails)
+(parse (p/eq '=) (=) :returns =)
+(parse (p/eq '=) (= 7) :leaves (7))
 
 (finalize)
